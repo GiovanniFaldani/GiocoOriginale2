b3b366e4a14baa709405e08edf81df03969c4e67
Giovanni Faldani <faldanig@yahoo.it>
2025-05-16 13:57:19 +0200

    FEATURE: check for path to destination
    Added legacy input system and AI package to project

---
+++ Assets/Scripts/ReachabilityTest.cs
@@ -0,0 +1,45 @@
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ReachabilityTest : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent spawnPosition;
+    [SerializeField] private Transform targetPosition;
+    [SerializeField] private NavMeshSurface navMesh;
+
+    [HideInInspector]
+    public bool pathAvailable = false;
+    public NavMeshPath navMeshPath;
+
+    private void Start()
+    {
+        navMeshPath = new NavMeshPath();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            navMesh.BuildNavMesh();
+            if (CalculateNewPath() == true)
+            {
+                pathAvailable = true;
+                Debug.Log("Path available");
+            }
+            else
+            {
+                pathAvailable = false;
+                Debug.Log("Path not available");
+            }
+        }
+    }
+
+    private bool CalculateNewPath()
+    {
+        spawnPosition.CalculatePath(targetPosition.position, navMeshPath);
+        if (navMeshPath.status == NavMeshPathStatus.PathComplete) return true;
+        return false;
+    }
+}
+
